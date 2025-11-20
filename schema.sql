dde670a92490b33c6b6b35759a9484eb290a6475
aic-fth <24-37186@g.batstate-u.edu.ph>
2025-11-20 08:54:40 +0800

    Group 7 Created Subject's Table

--- schema.sql
+++ schema.sql
@@ -5,3 +5,4 @@ CREATE TABLE classes ( class_id INT PRIMARY KEY, class_name VARCHAR(50), room_no
 CREATE TABLE enrollments ( enroll_id INT PRIMARY KEY, student_id INT, class_id INT, FOREIGN KEY (student_id) REFERENCES students(student_id), FOREIGN KEY (class_id) REFERENCES classes(class_id) );
 CREATE TABLE grades ( grade_id INT PRIMARY KEY, student_id INT, subject VARCHAR(50), grade FLOAT, FOREIGN KEY (student_id) REFERENCES students(student_id);
 CREATE TABLE departments (dept_id INT PRIMARY KEY, dept_name VARCHAR(50));
+CREATE TABLE subjects (subject_id INT PRIMARY KEY, subject_name VARCHAR(50), dept_id INT, FOREIGN KEY (dept_id) REFERENCES departments(dept_id);
